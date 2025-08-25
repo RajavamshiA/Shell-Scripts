52006d483a35940e40a3c8f7cf98579525fcc782
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-08-25 19:28:56 +0530

    Sum of numbers

--- 04.array.sh
+++ 04.array.sh
@@ -4,7 +4,7 @@ arr=("vamshi","raja","ankam")
 
 echo "first element in array is ${arr[0]}"
 echo "Second element in array is ${arr[1]}"
-echo "Third element in array is ${arr[3]}"
+echo "Third element in array is ${arr[2]}"
 
 echo "All elements in array are ${arr[@]}"
 
