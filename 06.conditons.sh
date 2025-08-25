0dc48a48af6abf5f9a84a6b1398ee711faa2a260
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-08-25 19:46:48 +0530

    conditions in shell script

--- 06.conditons.sh
+++ 06.conditons.sh
@@ -3,10 +3,8 @@
 number=$1
 
 if  [ $number gt 100 ]
-    {
-        echo "$number is greater than 100"
-    }
-else {
+then
+    echo "$number is greater than 100"
+else 
     echo "$number is less than 100"
-}
 fi
