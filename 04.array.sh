be2cc70c97b104e1ca9ee7bb6dcd69a30c5e45ee
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-08-25 19:30:45 +0530

    Sum of numbers

--- 04.array.sh
+++ 04.array.sh
@@ -8,3 +8,12 @@ echo "Third element in array is ${arr[2]}"
 
 echo "All elements in array are ${arr[@]}"
 
+
+MOVIES=("pushpa" "rrr" "devara")
+# index starts from 0, size is 3
+
+echo "First movie: ${MOVIES[0]}"
+echo "Second movie: ${MOVIES[1]}"
+echo "Third movie: ${MOVIES[2]}"
+
+echo "All movies are: ${MOVIES[@]}"
